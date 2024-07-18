25bde45da4f76ad58cab1e674d4b17a12335e2a4
Abhik004 <abhiksamanta004@gmail.com>
2024-07-18 18:46:25 +0530

    updated

--- app/SelfImprovment/page.tsx
+++ app/SelfImprovment/page.tsx
@@ -1,12 +1,10 @@
 "use client";
 import React from "react";
 import Image from "next/image";
-import Fade from 'react-awesome-reveal';
-import { useAnimation, motion } from "framer-motion";
-import { useInView } from "react-intersection-observer";
+import { motion } from "framer-motion";
 import Reveal from "react-awesome-reveal";
 import { keyframes } from "@emotion/react";
-import { selfImprovementData, SelfImprovementItem } from './data'; // Import data and types
+import { selfImprovementData, SelfImprovementItem } from "./data"; // Import data and types
 
 const customAnimation = keyframes`
   from {
@@ -29,14 +27,14 @@ const SelfImprovement = () => {
           initial="hidden"
           whileInView="visible"
           viewport={{ once: true }}
-          transition={{ duration: 1.9, type: "spring", bounce: "0.1" }}
+          transition={{ duration: 1.9, type: "spring", bounce: 0.1 }}
           variants={{
             visible: { opacity: 1, scale: 1, y: 0, x: 0 },
             hidden: { opacity: 0, scale: 0, x: "-50%" },
           }}
         >
           <div className="text-xl font-bold text-black">
-            What's wrong with self-improvement & how we&apos;re fixing it.
+            What&apos;s wrong with self-improvement & how we&apos;re fixing it.
           </div>
           <div className="text-6xl pb-3 font-bold text-black">
             Self Improvement. Ugh.
@@ -68,24 +66,27 @@ const SelfImprovement = () => {
         </motion.div>
       </div>
 
-      <div className="timeline px-20 pt-14 h-96 mt-10"  style={{ overflowY: 'scroll', WebkitOverflowScrolling: 'touch', scrollbarWidth: 'none' }}> {/*to hide the scrollbar*/}
+      <div className="timeline px-20 pt-14 h-96 mt-10" style={{ overflowY: 'scroll', WebkitOverflowScrolling: 'touch', scrollbarWidth: 'none' }}>
         <div className="border-l-2 border-l-violet-300 flex flex-col gap-16">
-        <Reveal keyframes={customAnimation}>
-          {selfImprovementData.map((item: SelfImprovementItem, index: number) => (
-            <motion.div
-              key={index}
-              initial="from"
-              animate="to"
-              variants={{customAnimation}}
-              transition={{ duration: 1.5, ease: "easeInOut" }}
-              className="max-w-[40rem] relative align-top pl-8"
-            >
-              <div style={{ left: "-12px" }} className="absolute">🟣</div>
-              <p className="text-xl font-bold">{item.title}</p>
-              <p className="mt-3">{item.content}</p>
-            </motion.div>
-          ))}
-        </Reveal>
+          <Reveal keyframes={customAnimation}>
+            {selfImprovementData.map((item: SelfImprovementItem, index: number) => (
+              <motion.div
+                key={index}
+                initial="from"
+                animate="to"
+                variants={{
+                  from: { opacity: 0.1, transform: "translate2d(-50px, 0)" },
+                  to: { opacity: 1, transform: "translate2d(0, 0)" },
+                }}
+                transition={{ duration: 1.5, ease: "easeInOut" }}
+                className="max-w-[40rem] relative align-top pl-8"
+              >
+                <div style={{ left: "-12px" }} className="absolute">🟣</div>
+                <p className="text-xl font-bold">{item.title}</p>
+                <p className="mt-3">{item.content}</p>
+              </motion.div>
+            ))}
+          </Reveal>
         </div>
       </div>
     </section>
