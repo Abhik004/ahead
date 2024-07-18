da71b256aaade0fdc19efd4498a07ece4b61e398
Abhik004 <abhiksamanta004@gmail.com>
2024-07-18 18:39:44 +0530

    updated

--- app/SelfImprovment/page.tsx
+++ app/SelfImprovment/page.tsx
@@ -1,7 +1,9 @@
-"use client"
+"use client";
 import React from "react";
 import Image from "next/image";
-import { motion } from "framer-motion";
+import Fade from 'react-awesome-reveal';
+import { useAnimation, motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 import Reveal from "react-awesome-reveal";
 import { keyframes } from "@emotion/react";
 import { selfImprovementData, SelfImprovementItem } from './data'; // Import data and types
@@ -24,21 +26,37 @@ const SelfImprovement = () => {
       <div className="flex flex-row justify-between items-center">
         <motion.div
           className="inline-block"
-          initial={{ opacity: 0, scale: 0 }}
-          animate={{ opacity: 1, scale: 1 }}
-          transition={{ duration: 1.9, type: "spring", bounce: 0.1 }}
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true }}
+          transition={{ duration: 1.9, type: "spring", bounce: "0.1" }}
+          variants={{
+            visible: { opacity: 1, scale: 1, y: 0, x: 0 },
+            hidden: { opacity: 0, scale: 0, x: "-50%" },
+          }}
         >
           <div className="text-xl font-bold text-black">
-            What&apos;s wrong with self-improvement & how we&apos;re fixing it.
+            What's wrong with self-improvement & how we&apos;re fixing it.
           </div>
           <div className="text-6xl pb-3 font-bold text-black">
             Self Improvement. Ugh.
           </div>
         </motion.div>
         <motion.div
-          initial={{ opacity: 0, x: -50 }}
-          animate={{ opacity: 1, x: 0 }}
+          viewport={{ once: true }}
           transition={{ duration: 2.1, ease: "easeInOut", type: "spring", delay: 0.3 }}
+          initial="hidden"
+          whileInView="visible"
+          variants={{
+            visible: {
+              opacity: 1,
+              scale: 1,
+              x: [0, -100, -300, -450, -500],
+              y: [35, 20, 10, 0, -10],
+              rotate: [-60, -45, -35, -35, -35, -35, -15],
+            },
+            hidden: { opacity: 0, scale: 0 },
+          }}
           className="flex items-center justify-center"
         >
           <Image
@@ -50,23 +68,24 @@ const SelfImprovement = () => {
         </motion.div>
       </div>
 
-      <div className="timeline px-20 pt-14 h-96 mt-10" style={{ overflowY: 'scroll', WebkitOverflowScrolling: 'touch', scrollbarWidth: 'none' }}>
+      <div className="timeline px-20 pt-14 h-96 mt-10"  style={{ overflowY: 'scroll', WebkitOverflowScrolling: 'touch', scrollbarWidth: 'none' }}> {/*to hide the scrollbar*/}
         <div className="border-l-2 border-l-violet-300 flex flex-col gap-16">
-          <Reveal keyframes={customAnimation}>
-            {selfImprovementData.map((item: SelfImprovementItem, index: number) => (
-              <motion.div
-                key={index}
-                initial={{ opacity: 0, x: -50 }}
-                animate={{ opacity: 1, x: 0 }}
-                transition={{ duration: 1.5, ease: "easeInOut" }}
-                className="max-w-[40rem] relative align-top pl-8"
-              >
-                <div style={{ left: "-12px" }} className="absolute">🟣</div>
-                <p className="text-xl font-bold">{item.title}</p>
-                <p className="mt-3">{item.content}</p>
-              </motion.div>
-            ))}
-          </Reveal>
+        <Reveal keyframes={customAnimation}>
+          {selfImprovementData.map((item: SelfImprovementItem, index: number) => (
+            <motion.div
+              key={index}
+              initial="from"
+              animate="to"
+              variants={{customAnimation}}
+              transition={{ duration: 1.5, ease: "easeInOut" }}
+              className="max-w-[40rem] relative align-top pl-8"
+            >
+              <div style={{ left: "-12px" }} className="absolute">🟣</div>
+              <p className="text-xl font-bold">{item.title}</p>
+              <p className="mt-3">{item.content}</p>
+            </motion.div>
+          ))}
+        </Reveal>
         </div>
       </div>
     </section>
